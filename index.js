96c3db46f72ad625870062814151228ca475d0a4
Andrea Freeman <afreeman5566@gmail.com>
2021-09-28 21:19:57 -0500

    commit tree c3d2c92ef7ff3fa42260df813a2bb3ce080e3349

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+
+
+  fetch("https://anapioficeandfire.com/api/books")
+  .then(function(response){
+    return response.json()
+  })
+  .then(function(json){
   
+  
+   return renderBooks(json)
+  })
+  
+  return fetchBooks 
 }
+
+
+
+
+
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
